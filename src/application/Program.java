ee18c4869fb6d835d49131042885aa16d4f4dbdf
Jean Diniz <jean.diniz@youxgroup.com.br>
2021-12-14 14:56:46 -0300

    Clear no console e tratando exceptions

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,12 +2,41 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessException;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+
+        while(true) {
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Local de origem da peça: ");
+                ChessPosition source = UI.readChessPosition(input);
+
+                System.out.println();
+                System.out.print("Local de destino da peça: ");
+                ChessPosition target = UI.readChessPosition(input);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException e) {
+                System.out.println(e.getMessage() + "\nAperte ENTER para tentar novamente.");
+                input.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage() + "\nAperte ENTER para tentar novamente. ");
+                input.nextLine();
+            }
+
+        }
     }
 }
