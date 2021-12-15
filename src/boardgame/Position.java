8c5d012b79e49a444a85ad716cded9f5e7c17170
Jean Diniz <jean.diniz@youxgroup.com.br>
2021-12-15 09:07:56 -0300

    movimentos da torre

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -22,8 +22,13 @@ public class Position {
         return column;
     }
 
-    public void setColumn(int color) {
-        this.column = color;
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public void setValues(int row, int column) {
+        this.row = row;
+        this.column = column;
     }
 
     @Override
